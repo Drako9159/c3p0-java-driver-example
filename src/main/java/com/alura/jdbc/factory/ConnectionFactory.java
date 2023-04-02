8190c09e957fa91179c274b5401e216626d86931
drako9159 <chochito9159@gmail.com>
2023-04-02 01:05:06 -0600

    add depedency for c3p0 controller

--- src/main/java/com/alura/jdbc/factory/ConnectionFactory.java
+++ src/main/java/com/alura/jdbc/factory/ConnectionFactory.java
@@ -1,16 +1,26 @@
 package com.alura.jdbc.factory;
-
+import com.mchange.v2.c3p0.ComboPooledDataSource;
+import javax.sql.DataSource;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class ConnectionFactory {
-    public Connection recuperaConexion() throws SQLException {
-        return DriverManager.getConnection(
+    private DataSource datasource;
+    public ConnectionFactory (){
+        var poolDataSource = new ComboPooledDataSource();
+        poolDataSource.setJdbcUrl("jdbc:mysql://localhost/control_de_stock?useTimeZone=true&serverTimeZone=UTC");
+        poolDataSource.setUser("root");
+        poolDataSource.setPassword("password");
 
+        this.datasource = poolDataSource;
+    }
+    public Connection recuperaConexion() throws SQLException {
+        return this.datasource.getConnection();
+        /*
+                DriverManager.getConnection(
                 "jdbc:mysql://localhost/control_de_stock?useTimeZone=true&serverTimeZone=UTC",
                 "root",
-                "password");
-
+                "password");*/
     }
 }
